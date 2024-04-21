32dc517396b904f169b43c95d799530cbc41ed25
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-04-21 22:43:10 +0300

    adds periodical feeding parameters

--- esp32/pet_feeder_mqtt/config.example.h
+++ esp32/pet_feeder_mqtt/config.example.h
@@ -1,8 +1,13 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char* ssid = "YOUR_SSID";
-const char* password = "YOUR_SSID_PASSWORD";
-const char* mqttServer = "YOUR_MQTT_SERVER_IP";
+const char *ssid = "YOUR_SSID";
+const char *password = "YOUR_SSID_PASSWORD";
+const char *mqttServer = "YOUR_MQTT_SERVER_IP";
+int GTMOffset = 3; // Timezone
+char feeding_type[] = "periodical";
+int feeding_period = 5; // Once a every 5 hours
+int default_periodical_feeding_amount = 1;
+int default_mqtt_feeding_amount = 1;
 
 #endif
