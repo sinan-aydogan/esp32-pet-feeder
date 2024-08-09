88c2201fb5ec55655ae503e2fe687d5557f462ea
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-08-09 17:10:01 +0300

    heart beat methodunu günceller

--- esp32/pet_feeder_mqtt/pet_feeder_mqtt.ino
+++ esp32/pet_feeder_mqtt/pet_feeder_mqtt.ino
@@ -8,7 +8,7 @@
 
 // NTP Client Settings
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", GTMOffset*60*60, 36000);
+NTPClient timeClient(ntpUDP, "pool.ntp.org", GTMOffset * 60 * 60, 36000);
 
 // Mqtt
 WiFiClient espClient;
@@ -18,7 +18,7 @@ const char* feed_topic = "pet_feeder/feed";
 const char* heart_beat_topic = "pet_feeder/heart_beat";
 
 // Servo
-Servo myservo; 
+Servo myservo;
 int servoPin = 18;
 
 // Heart-beat
@@ -29,11 +29,11 @@ int last_feeding = 0;
 int feeding_counter = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Servo
   myservo.setPeriodHertz(50);
-  myservo.attach(servoPin,1000,2000);
+  myservo.attach(servoPin, 1000, 2000);
   myservo.writeMicroseconds(1500);
 
   // Network
@@ -62,42 +62,53 @@ void loop() {
 
 
 void connect_wifi() {
-  if (!WiFi.isConnected()) {
-    Serial.println("Connecting to WiFi...");
-    strcpy(feeding_type, "periodical");
-    
-    WiFi.begin(ssid, password);
-    while (!WiFi.isConnected()) {
-      delay(500);
-      Serial.print(".");
+  delay(500);
+  WiFi.disconnect();
+  delay(500);
+  unsigned long connectionCounter = 0;
+  Serial.println("Connecting to WiFi...");
+
+  WiFi.begin(ssid, password);
+  while (WiFi.isConnected() == 0) {
+    connectionCounter++;
+    delay(500);
+    Serial.print(".");
+    if (connectionCounter >= 30) {
+      Serial.println("Card restarting...");
+      ESP.restart();
+      connectionCounter = 0;
     }
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
   }
-  delay(1000);
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(5000);
   setup_mqtt();
+  delay(2000);
+  String success_message = "Device connected: " + WiFi.localIP().toString() + "  -  " + timeClient.getFormattedTime();
+  client.publish("pet_feeder/status", success_message.c_str());
 }
 
-void wifi_reconnect(){
-  unsigned long currentMillis = millis();
-  if (!WiFi.isConnected() && (currentMillis - previousMillis >=3000)) {
-    Serial.print(millis());
+void wifi_reconnect() {
+  if (int(WiFi.isConnected()) == 6) {
+    strcpy(feeding_type, "periodical");
     Serial.println("Reconnecting to WiFi...");
     WiFi.disconnect();
-    WiFi.reconnect();
-    previousMillis = currentMillis;
-}
+    connect_wifi();
+  }
 }
 
 // Mqtt methods
-void setup_mqtt(){
+void setup_mqtt() {
   client.setServer(mqttServer, 1883);
   mqtt_reconnect();
   client.setCallback(mqtt_callback);
 }
 
 void mqtt_reconnect() {
+  unsigned long currentMillis = millis();
+  client.disconnect();
+  delay(500);
   while (!client.connected()) {
     strcpy(feeding_type, "periodical");
     Serial.println("Attempting MQTT connection...");
@@ -106,11 +117,16 @@ void mqtt_reconnect() {
       Serial.println("Connected");
       Serial.println("Subscribed pet_feeder/feed topic");
       client.subscribe("pet_feeder/feed");
+      publish_heart_beat();
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
       delay(5000);
+      if (!WiFi.isConnected() && (currentMillis - previousMillis >= 15000)) {
+        setup_mqtt();
+        wifi_reconnect();
+      }
     }
   }
 }
@@ -125,41 +141,49 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   deserializeJson(doc, payload, length);
 
   // Feed
-  if(strcmp(topic, feed_topic) == 0){
+  if (strcmp(topic, feed_topic) == 0) {
     handle_feed(doc);
   }
 }
 
 // Servo methods
 void rotateServo(int turn) {
-  for(int i = 0; i < turn; i++){
+  for (int i = 0; i < turn; i++) {
     feeding_counter++;
     Serial.println(turn);
+    myservo.writeMicroseconds(2000);
+    delay(150);
     myservo.writeMicroseconds(1000);
-    delay(300);
+    delay(150);
+    myservo.writeMicroseconds(1000);
+    delay(350);
+    myservo.writeMicroseconds(2000);
+    delay(150);
+    myservo.writeMicroseconds(1000);
+    delay(150);
     myservo.writeMicroseconds(1500);
     delay(1000);
   }
 }
 
 // Feed method
-void handle_feed(JsonDocument &doc) {
-  String time = doc["time"] ? doc["time"] :  timeClient.getFormattedTime();
+void handle_feed(JsonDocument& doc) {
+  String time = doc["time"] ? doc["time"] : timeClient.getFormattedTime();
   int amount = doc["amount"] ? doc["amount"] : default_mqtt_feeding_amount;
 
   rotateServo(amount);
   String response = "Feeding completed, " + String(amount) + " meal/s";
   Serial.println(response.c_str());
-  client.publish("pet_feeder/feed/reponse", response.c_str());
+  client.publish("pet_feeder/status", response.c_str());
 }
 
 // Heart-beat method
 void heart_beat_timer() {
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= 10000) {
+  if (currentMillis - previousMillis >= (heart_beat_period * 60 * 1000)) {
     previousMillis = currentMillis;
-      publish_heart_beat();
+    publish_heart_beat();
   }
 }
 
@@ -179,16 +203,16 @@ void publish_heart_beat() {
   client.publish(heart_beat_topic, buffer);
 }
 
-void check_feeding(){
+void check_feeding() {
   unsigned long currentMillis = millis();
-  if((strcmp(feeding_type, "periodical") == 0) || !client.connected()){
-    int period = feeding_period*60*60*1000;
-      if(currentMillis - last_feeding >= period){
-        Serial.println(currentMillis);
-        Serial.println(last_feeding);
-        Serial.println(period);
-        last_feeding = currentMillis;
-        rotateServo(default_periodical_feeding_amount);
-      }
+  if ((strcmp(feeding_type, "periodical") == 0) || !client.connected()) {
+    int period = feeding_period * 60 * 60 * 1000;
+    if (currentMillis - last_feeding >= period) {
+      Serial.println(currentMillis);
+      Serial.println(last_feeding);
+      Serial.println(period);
+      last_feeding = currentMillis;
+      rotateServo(default_periodical_feeding_amount);
+    }
   }
 }
--- web/resources/js/Components/ConfirmsPassword.vue
+++ web/resources/js/Components/ConfirmsPassword.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive, nextTick } from 'vue';
+import {ref, reactive, nextTick} from 'vue';
 import DialogModal from './DialogModal.vue';
 import InputError from './InputError.vue';
 import PrimaryButton from './PrimaryButton.vue';
@@ -73,7 +73,7 @@ const closeModal = () => {
 <template>
     <span>
         <span @click="startConfirmingPassword">
-            <slot />
+            <slot/>
         </span>
 
         <DialogModal :show="confirmingPassword" @close="closeModal">
@@ -95,7 +95,7 @@ const closeModal = () => {
                         @keyup.enter="confirmPassword"
                     />
 
-                    <InputError :message="form.error" class="mt-2" />
+                    <InputError :message="form.error" class="mt-2"/>
                 </div>
             </template>
 
